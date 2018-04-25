2c0d66679e1e2fc7227a517068d0f8707359caa9
Antonio Peric-Mazar <antonio@locastic.com>
2018-04-25 19:53:59 +0200

    Changed namaspaces to LocasticSyliusHTPayWayPlugin

--- tests/Behat/Context/Ui/Shop/WelcomeContext.php
+++ tests/Behat/Context/Ui/Shop/WelcomeContext.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Tests\Acme\SyliusExamplePlugin\Behat\Context\Ui\Shop;
+namespace Tests\Locastic\SyliusHTPayWayPlugin\Behat\Context\Ui\Shop;
 
 use Behat\Behat\Context\Context;
-use Tests\Acme\SyliusExamplePlugin\Behat\Page\Shop\WelcomePageInterface;
+use Tests\Locastic\SyliusHTPayWayPlugin\Behat\Page\Shop\WelcomePageInterface;
 use Webmozart\Assert\Assert;
 
 final class WelcomeContext implements Context
